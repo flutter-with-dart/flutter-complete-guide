702fd6050032ec4a18b02af0ed701b3bb744de32
AzzarRizky <azzar.arza18@gmail.com>
2022-11-14 10:00:29 +0700

    add override

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello'),
