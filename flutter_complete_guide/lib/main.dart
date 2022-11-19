7cb77dc96b01add77411ca5f98604b4681efeaf1
AzzarRizky <azzar.arza18@gmail.com>
2022-11-19 18:51:17 +0700

    Update main.dart

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -4,7 +4,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      // home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This Is My Default Text'),
+      ),
     );
   }
 }
