21c6e3b731f35a478a3ad502656998219e096f64
AzzarRizky <azzar.arza18@gmail.com>
2022-11-19 20:41:17 +0700

    Update main.dart

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -3,13 +3,34 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour',
+      'What\'s your favourite Animal?',
+    ];
+
     return MaterialApp(
       // home: Text('Hello'),
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This Is My Default Text'),
+        body: Column(
+          children: [
+            Text('The Questions!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
